de015a8b64cb5115bca3adb57f6952fbb32b107d
jackalpsh <tkdgus2918@naver.com>
2018-06-12 13:16:25 +0900

    인사인사 두번째 커밋

--- 03_HelloGit/src/com/kh/git/Test1.java
+++ 03_HelloGit/src/com/kh/git/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕 깃");
+		System.out.println("hello 깃");
+		System.out.println("니하오 깃");
 		//gitignore.io
 	}
 
