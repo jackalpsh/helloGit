ef02eb9fb421e818faaf24eb891c12b7137726ff
jackalpsh <tkdgus2918@naver.com>
2018-06-12 13:17:53 +0900

    테스트투

---
+++ 03_HelloGit/src/com/kh/git/Test2.java
@@ -0,0 +1,11 @@
+package com.kh.git;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" 깃으로 본 세상");
+		//gitignore.io
+	}
+
+}
