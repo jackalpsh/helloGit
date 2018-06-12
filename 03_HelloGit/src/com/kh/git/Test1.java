b56e63538175fc1ebf6cb5dff8202d8f920d8fdf
jackalpsh <tkdgus2918@naver.com>
2018-06-12 13:03:25 +0900

    헬로우 깃 프로젝트 생성 두번째 줄은 비우고
    반갑다 깃

---
+++ 03_HelloGit/src/com/kh/git/Test1.java
@@ -0,0 +1,11 @@
+package com.kh.git;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕 깃");
+		//gitignore.io
+	}
+
+}
